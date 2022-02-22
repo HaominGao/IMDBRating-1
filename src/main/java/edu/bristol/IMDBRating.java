ecb64b699c48e73e640b155769c5725588176c00
HG <px21457@bristol.ac.uk>
2022-02-22 23:05:58 +0000

    Changed message

--- src/main/java/edu/bristol/IMDBRating.java
+++ src/main/java/edu/bristol/IMDBRating.java
@@ -4,9 +4,13 @@ public class IMDBRating
 {
     private float currentAverage = 0;
     private int ratingCount = 0;
+    private float falseNewRating = -1;
 
     public float addNewRating(int newRating)
     {
+        if((newRating > 10)||(newRating < 0)){
+            return falseNewRating;
+        }
         int previousTotal = (int) (currentAverage * ratingCount);
         int newTotal = previousTotal + newRating;
         ratingCount++;
